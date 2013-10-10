d0f23efdc80f9b78bad895242fef3a86ef409e9a
mathyomama <mathyomama@gmail.com>
2013-10-10 00:10:33 -0400

    wrote some code for splitting up the file received from wiktionary

--- online-version/test.py
+++ online-version/test.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
 
+import re
 import urllib2
 
 data = urllib2.urlopen("https://en.wiktionary.org/w/index.php?action=raw&title=example")
-source = data.read()
 
-print source
+equal = re.compile(r"=+.*=+")
+
+for line in data:
+	if equal.match(line)==None:
+		continue
+	else:
+		print equal.match(line).group()
