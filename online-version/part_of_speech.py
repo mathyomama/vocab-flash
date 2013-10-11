c1922ed4fd7a97baa459525eb8bc38145cf3b765
mathyomama <mathyomama@gmail.com>
2013-10-10 20:57:42 -0400

    Saving my game

---
+++ online-version/part_of_speech.py
@@ -0,0 +1,58 @@
+from section import PartOfSpeech
+
+
+class Noun(PartOfSpeech):
+	"""A class for the section labeled 'noun'"""
+
+	def __init__(self, heading, information):
+		super(Noun, self).__init__(heading, information)
+
+
+class Verb(PartOfSpeech):
+	"""A class for the section labeled 'noun'"""
+
+	def __init__(self, heading, information):
+		super(Verb, self).__init__(heading, information)
+
+
+class Adjective(PartOfSpeech):
+	"""A class for the section labeled 'noun'"""
+
+	def __init__(self, heading, information):
+		super(Adjective, self).__init__(heading, information)
+
+
+class Adverb(PartOfSpeech):
+	"""A class for the section labeled 'noun'"""
+
+	def __init__(self, heading, information):
+		super(Adverb, self).__init__(heading, information)
+
+
+class Pronoun(PartOfSpeech):
+	"""A class for the section labeled 'noun'"""
+
+	def __init__(self, heading, information):
+		super(Pronoun, self).__init__(heading, information)
+
+
+class Preposition(PartOfSpeech):
+	"""A class for the section labeled 'noun'"""
+
+	def __init__(self, heading, information):
+		super(Prepostion, self).__init__(heading, information)
+
+
+class Conjunction(PartOfSpeech):
+	"""A class for the section labeled 'noun'"""
+
+	def __init__(self, heading, information):
+		super(Conjunction, self).__init__(heading, information)
+
+
+class Interjection(PartOfSpeech):
+	"""A class for the section labeled 'noun'"""
+
+	def __init__(self, heading, information):
+		super(Interjection, self).__init__(heading, information)
+
--- online-version/test.py
+++ online-version/test.py
@@ -4,12 +4,45 @@
 import re
 import urllib2
 
-data = urllib2.urlopen("https://en.wiktionary.org/w/index.php?action=raw&title=example")
+#data = urllib2.urlopen("https://en.wiktionary.org/w/index.php?action=raw&title=example")
+#
+#equal = re.compile(r"=+.*=+")
+#
+#for line in data:
+#	if equal.match(line)==None:
+#		continue
+#	else:
+#		print equal.match(line).group()
 
-equal = re.compile(r"=+.*=+")
 
-for line in data:
-	if equal.match(line)==None:
-		continue
-	else:
-		print equal.match(line).group()
+class Shape(object):
+	def __init__(self, shapename):
+		self.shapename = shapename
+
+
+class ColoredShape(Shape):
+	def __init__(self, color, shapename):
+		self.color = color
+		super(ColoredShape, self).__init__(shapename)
+		self.something = self.shapename
+
+
+cs = ColoredShape('red', 'circle')
+print cs.color, cs.shapename, cs.something
+
+cs1 = ColoredShape('red', shapename='circle')
+print cs1.color, cs1.shapename
+
+cs2 = ColoredShape(color='red', shapename='circle')
+print cs2.color, cs2.shapename
+
+cs3 = ColoredShape(shapename='circle', color='red')
+print cs3.color, cs3.shapename
+
+
+
+def foo(**kwargs):
+	print kwargs
+
+
+foo(dog='cat', cat='mouse', mouse='cheese')
