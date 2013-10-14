45d0a2d53d5de355dd58fd20c9daf3c62d37f05f
mathyomama <mathyomama@gmail.com>
2013-10-13 22:11:27 -0400

    Changed parser.py to parse.py and moved all the contents of section.py to parse.py

--- online-version/defi.py
+++ online-version/defi.py
@@ -1,19 +1,19 @@
-import parser
+import parse
 import urllib2
 import error
 
 
 
-class definition(object):
+class Definition(object):
 	"""This class takes a word and uses urllib2 to grab a file which is then parsed using Parse from parser."""
 
 
 	def __init__(self, word):
 		try:
 			self.data = urllib2.urlopen("http://en.wiktionary.org/w/index.php?action=raw&title=%s" % word)
-			self.entry = parser.Parse(self.data)
-		except(urllib2.HTTPError):
-			self.http_error_message()			
+			self.entry = parse.Parser(self.data)
+		except urllib2.HTTPError:
+			self.http_error_message()
 
 	def check_word(self, data):
 		pass
--- online-version/vocab-flash.py
+++ online-version/vocab-flash.py
@@ -8,7 +8,9 @@ def main():
 	print "What word would you like to define?"
 	word = raw_input("> ")
 	my_definition = defi.definition(word)
-	print my_definition.entry.get_headings()
+	print my_definition
+	if my_definition != None:
+		print my_definition.entry.get_headings()
 
 if __name__ == "__main__":
 	main()
